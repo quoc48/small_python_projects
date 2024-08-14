3e60ae171e3d49be870ac31d5b81782a21fb1684
quocphan <quocphan@example.com>
2024-08-14 22:59:27 +0700

    in coding

---
+++ birthday_paradox/birthdayparadox.py
@@ -0,0 +1,33 @@
+""" Birthday Paradox Simulation
+Explore the surprising probabilities of the "Birthday Paradox"
+"""
+
+import datetime, random
+
+
+def getBirthdays(numberOfBirthdays):
+    """Return a list of number random date object for birthdays."""
+    birthdays = []
+
+    for i in range(numberOfBirthdays):
+        # The year is unimportant for our simulation, as long as all
+        # birthdays have the same year.
+        startOfYear = datetime.date(2001, 1, 1)
+
+        # Get a random day into the year:
+        randomNumberOfDays = datetime.timedelta(random.randint(0, 364))
+        birthday = startOfYear + randomNumberOfDays
+        birthdays.append(birthday)
+    return birthdays
+
+def getMatch(birthdays):
+    """Returns the date object of a birthday that occurs more than once
+    in the birthday list."""
+    if len(birthdays) == len(set(birthdays)):
+        return None  # All birthdays are unique, so return None.
+
+    # Compare each birthday to every other birthday:
+    for a, birthdayA in enumerate(birthdays):
+        for b, birthdayB in enumerate(birthdays[a + 1 :]):
+            if birthdayA == birthdayB:
+                return birthdayA  # Return the matching birthday.
