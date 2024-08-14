e640846fe9ffbf2fa0cc10eccf09e8f869377ebf
quocphan <quocphan@example.com>
2024-08-14 23:08:44 +0700

    adding intro

--- birthday_paradox/birthdayparadox.py
+++ birthday_paradox/birthdayparadox.py
@@ -31,3 +31,11 @@ def getMatch(birthdays):
         for b, birthdayB in enumerate(birthdays[a + 1 :]):
             if birthdayA == birthdayB:
                 return birthdayA  # Return the matching birthday.
+
+# Display the intro:
+print('''The Birthday Paradox shows us that in a group of N people, the odds
+that two of them have matching birthdays is surprisingly large.
+This program does a Monte Carlo simulation (that is, repeated random
+simulations) to explore this concept.
+
+(It's not actually a paradox, it's just a surprising result.)''')
