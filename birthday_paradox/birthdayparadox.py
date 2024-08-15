ba0483cc432959acd53c8ee042587a83e506b703
quocphan <quocphan@example.com>
2024-08-15 21:03:08 +0700

    coding

--- birthday_paradox/birthdayparadox.py
+++ birthday_paradox/birthdayparadox.py
@@ -39,3 +39,29 @@ This program does a Monte Carlo simulation (that is, repeated random
 simulations) to explore this concept.
 
 (It's not actually a paradox, it's just a surprising result.)''')
+
+# Set up a tuple of month names in order:
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+          'Otc', 'Nov', 'Dec')
+
+while True: # Keep asking until the user enters a valid amount.
+    print("How many birthdays shall I generate? (Max 100)")
+    response = input('> ')
+    if response.isdecimal() and (0 < int(response) <= 100):
+        numBDays = int(response)
+        break   # User has entered a valid amount.
+print()
+
+# Generate and display the birthdays:
+print('Here are', numBDays, 'birthdays:')
+birthdays = getBirthdays(numBDays)
+for i, birthday in enumerate(birthdays):
+    if i != 0:
+        # Display a comma for each birthday after the first birthday.
+        print(', ', end='')
+    monthName = MONTHS[birthday.month - 1]
+    dateText = '{} {}'.format(monthName, birthday.day)
+    print(dateText, end='')
+print()
+print()
+
