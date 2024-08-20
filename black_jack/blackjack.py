3e2d4fcede1a1de01f976f0e87771a27215c393d
quocphan <thanh.quoc731@gmail.com>
2024-08-20 21:42:07 +0700

    coding...

--- black_jack/blackjack.py
+++ black_jack/blackjack.py
@@ -6,3 +6,57 @@ DIAMONDS = chr(9830)
 SPADES = chr(9824)
 CLUBS = chr(9827)
 BACKSIDE = 'backside'
+
+def main():
+    print('''Blackjack
+    Rule:
+        Try to get as close to 21 without going over.
+        Kings, Queens, and Jacks are worth 10 points.
+        Aces are worth 1 or 11 points.
+        Cards 2 through 10 are worth their face value.
+        (H)it to take another card.
+        (S)tand to stop taking cards.
+        On your first play, you can (D)ouble down to increase your bet
+        but must hit exactly one more time before standing.
+        In case of a tie, the bet is returned to the player.
+        The dealer stops hitting at 17. 
+    ''')
+
+    money = 5_000
+    while True: # Main game loop.
+        # Check if the player has run out of money:
+        if money <= 0:
+            print("You're broke!")
+            print("Good thing you weren't playing with real money.")
+            print("Thanks for playing!")
+            sys.exit()
+
+        # Let the player enter their bet for this round:
+        print('Money:', money)
+        bet = getBet(money)
+
+        # Give the dealer and player two card from the deck each:
+        deck = getDeck()
+        dealerHand = [deck.pop(), deck.pop()]
+        playerHand = [deck.pop()], deck.pop()]
+
+        # Handle player actions:
+        print('Bet', bet)
+        while True: # Keep looping until player stands or busts.
+            displayHands(playerHand, dealerHand, False)
+            print()
+
+            # Check if the player has bust:
+            if getHandValue(playerHand) > 21:
+                break
+
+            # Get the player move, either H, S or D:
+            move = getMove(playerHand, money - bet)
+
+            # Handle the player actions:
+            if move == 'D':
+                # Player is doubling down, they can increase their bet:
+                additionalBet = getBet(min(bet, (money - bet)))
+                bet += additionalBet
+                print('Bet increased to {}.'.format(bet))
+                print('Bet:', bet)
