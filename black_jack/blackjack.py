7dc35245ba567db5ba34ebc191bd16e4d93fdc7b
quocphan <thanh.quoc731@gmail.com>
2024-08-22 22:24:16 +0700

    coding...

--- black_jack/blackjack.py
+++ black_jack/blackjack.py
@@ -75,3 +75,52 @@ def main():
             if move in ('S', 'D'):
                 # Stand/doubling down stops the player's turn.
                 break
+
+        # Handle the dealer's action:
+        if getHandValue(playerHand) <= 21:
+            while getHandValue(dealerHand) < 17:
+                # The dealer hits:
+                print('Dealer hits...')
+                dealerHand.append(deck.pop())
+                displayHands(playerHand, dealerHand, False)
+
+                if getHandValue(dealerHand) > 21:
+                    break   # The dealer has busted.
+                input('Press Enter to continue...')
+                print('\n\n')
+            # Show the final hands:
+            displayHands(playerHand, dealerHand, True)
+
+            playerValue = getHandValue(playerHand)
+            dealerValue = getHandValue(dealerHand)
+            # Handle whether the player won, lost, or tied:
+            if dealerValue > 21:
+                print('Dealer busts! You win ${}!'.format(bet))
+                money += bet
+            elif (playerValue > 21) or (playerValue < dealerValue):
+                print("You lost!")
+                money -= bet
+            elif playerValue > dealerValue:
+                print('You won ${}!'.format(bet))
+                money += bet
+            elif playerValue == dealerValue:
+                print('It\'s a tie, the bet is returned to you.')
+
+            input('Press Enter to continue...')
+            print('\n\n')
+
+def getBet(maxBet):
+    """Ask the player how much they want to bet for this round."""
+    while True: # Keep asking until they enter a valid amount.
+        print('How much do you bet? (1-{}, or QUIT)'.format(maxBet))
+        bet = input('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Thank for playing!')
+            sys.exit()
+
+        if not bet.isdecimal():
+            continue    # If the player didn't enter a number, ask again.
+
+        bet = int(bet)
+        if 1 <= bet <= maxBet:
+            return bet  # Player entered a valid bet.
