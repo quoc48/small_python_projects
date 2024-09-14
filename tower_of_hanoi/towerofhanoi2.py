0734eac409097de5fe54bb539bf758ae6e9b877b
quocphan <quoc48@users.noreply.github.com>
2024-09-14 23:09:45 +0700

    re-write the tower of hanoi program

--- tower_of_hanoi/towerofhanoi2.py
+++ tower_of_hanoi/towerofhanoi2.py
@@ -2,3 +2,30 @@ import copy, sys
 
 TOTAL_DISKS = 5
 
+# Start with all disks on tower A:
+COMPLETE_TOWER = list(range(TOTAL_DISKS, 0, -1))
+
+def main():
+    print(""" Move the tower of disks, one disk at a time, to another tower.
+        Larger disks cannot rest on top of a smaller disk.""")
+
+    # Set up the towers. The end of the list is the top of the tower.
+    towers = {'A': copy.copy(COMPLETE_TOWER), 'B': [], 'C': []}
+
+    while True:
+        # Display the towers and disks:
+        displayTowers(towers)
+
+        # Ask the user for a move:
+        fromTower, toTower = askForPlayerMove(towers)
+
+        # Move the top disk from fromTower to toTower:
+        disk = towers[fromTower].pop()
+        towers[toTower].append(disk)
+
+        # Check if the user has solved the puzzle:
+        if COMPLETE_TOWER in (towers['B'], towers['C']):
+            displayTowers(towers)
+            print('You have solved the puzzle! Well done!')
+            sys.exit()
+
