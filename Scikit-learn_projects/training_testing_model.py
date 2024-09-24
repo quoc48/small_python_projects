64fe0ef529bad75c2360cd0da62d21391ccbb271
quocphan <quoc48@users.noreply.github.com>
2024-09-24 23:46:42 +0700

    split train and test dataset

--- Scikit-learn_projects/training_testing_model.py
+++ Scikit-learn_projects/training_testing_model.py
@@ -1,3 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 iris_dataset = load_iris()
@@ -10,3 +15,10 @@ print("Y_train shape: {}".format(y_train.shape))
 
 print("X_test shape: {}".format(X_test.shape))
 print("Y_test shape: {}".format(y_test.shape))
+
+# create dataframe from data in X_train
+# label the columns using the strings in iris_dataset.feature_name
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_name)
+# create a scatter matrix from the dataframe, color by y_train
+grr = pd.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+                        hist_kwds={'blin': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
