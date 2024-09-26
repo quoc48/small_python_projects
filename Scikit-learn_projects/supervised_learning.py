397b84c98b770cc10784b82c0e60e8cc9c507055
quocphan <quoc48@users.noreply.github.com>
2024-09-26 23:59:36 +0700

    start supervised_learning task

--- Scikit-learn_projects/supervised_learning.py
+++ Scikit-learn_projects/supervised_learning.py
@@ -1,11 +1,16 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import mglearn
 
 # generate dataset
 X, y = mglearn.datasets.make_forge()
+
 # plot dataset
 mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+
 plt.legend(["Class 0", "Class 1"], loc=4)
 plt.xlabel("First feature")
 plt.ylabel("Second feature")
 print("X.shape: {}".format(X.shape))
+
+# Add this line to display the plot
+plt.show()
