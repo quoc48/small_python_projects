8d5cee6df5bf24dffe3c4cf98c45ec56e71e818b
quocphan <quoc48@users.noreply.github.com>
2024-10-01 22:01:37 +0700

    coding ridge regression

---
+++ Scikit-learn_projects/ridge_regression.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+
+X, y = load_diabetes(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+ridge = Ridge().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test)))
