d061015e3edeeee64038387bf08f09026fd8b100
quocphan <quoc48@users.noreply.github.com>
2024-10-02 22:20:38 +0700

    coding linear regression with multi classes

--- Scikit-learn_projects/linear_regression_multi.py
+++ Scikit-learn_projects/linear_regression_multi.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 import mglearn
 from sklearn.datasets import make_blobs
+from sklearn.svm import LinearSVC
 
 X, y = make_blobs(random_state=42)
 mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
 plt.xlabel("Feature 0")
 plt.ylabel("Feature 1")
 plt.legend(["Class 0", "Class 1", "Class 2"])
+
+linear_svm = LinearSVC().fit(X, y)
+print("Coefficient shape: ", linear_svm.coef_.shape)
+print("Intercept shape: ", linear_svm.intercept_.shape)
+plt.show()
