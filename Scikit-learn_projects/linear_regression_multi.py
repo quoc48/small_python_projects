2983dcb06a1b2d84c41e4c980b1d2f1857f3ace9
quocphan <quoc48@users.noreply.github.com>
2024-10-02 22:06:25 +0700

    coding linear regression with multi classes

---
+++ Scikit-learn_projects/linear_regression_multi.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import mglearn
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs(random_state=42)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+plt.legend(["Class 0", "Class 1", "Class 2"])
